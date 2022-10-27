bfc8bf5fda89f2c2048350dc8cb78969ab330c91
dev7355608 <dev7355608@gmail.com>
2022-10-27 16:58:58 +0200

    Update PreciseText

--- scripts/precise-text.js
+++ scripts/precise-text.js
@@ -26,18 +26,29 @@ PreciseText.prototype.updateText = function (respectDirty) {
     if (!respectDirty || this.dirty || this.localStyleID !== style.styleID) {
         const measured = PIXI.TextMetrics.measureText(this._text || " ", style, style.wordWrap, this.canvas);
         const size = Math.ceil(Math.max(measured.width, measured.height, 1) + style.padding * 2);
-        const maxSize = canvas.performance.textures.maxSize;
-        const maxZoom = Math.min(CONFIG.Canvas.maxZoom, 4);
+        const maxSize = PreciseText._MAX_TEXTURE_SIZE ?? 4096;
+        const maxZoom = PreciseText._MAX_ZOOM ?? 3;
+        const maxResolution = PreciseText._MAX_RESOLUTION ?? 2;
 
         this._resolution = Math.min(Math.max((maxSize / 2 - 1) / size, Math.min((maxSize - 1) / size, 2)), maxZoom) * this._quality;
         this._resolution *= Math.min((maxSize - 1) / Math.ceil(size * this._resolution), 1);
-
-        if (canvas.performance.mode <= CONST.CANVAS_PERFORMANCE_MODES.MED) {
-            this._resolution = Math.min(this._resolution, 2);
-        } else if (canvas.performance.mode <= CONST.CANVAS_PERFORMANCE_MODES.HIGH) {
-            this._resolution = Math.min(this._resolution, 3);
-        }
+        this._resolution = Math.min(this._resolution, maxResolution);
     }
 
     PIXI.Text.prototype.updateText.call(this, respectDirty);
 };
+
+Hooks.on("canvasInit", () => {
+    const gl = canvas.app.renderer.gl;
+
+    PreciseText._MAX_TEXTURE_SIZE = gl.getParameter(gl.MAX_TEXTURE_SIZE);
+    PreciseText._MAX_ZOOM = Math.min(CONFIG.Canvas.maxZoom, 4);
+
+    if (canvas.performance.mode <= CONST.CANVAS_PERFORMANCE_MODES.MED) {
+        PreciseText._MAX_RESOLUTION = 2;
+    } else if (canvas.performance.mode <= CONST.CANVAS_PERFORMANCE_MODES.HIGH) {
+        PreciseText._MAX_RESOLUTION = PreciseText._MAX_ZOOM > 3 ? 3 : 2;
+    } else {
+        PreciseText._MAX_RESOLUTION = PreciseText._MAX_ZOOM;
+    }
+});
